8579d4273b669494831600ef366ed6bd40124d85
Mark Ellis <markellisdev@gmail.com>
2016-11-23 08:11:53 -0600

    untracked movies displayed

--- js/main.js
+++ js/main.js
@@ -5,13 +5,24 @@ let signIn = require("./user");
 
 let Handlebars = require('hbsfy/runtime');
 let cardTemplate = require('../templates/onload.hbs');
+let untrackedResults = require("./Untracked.js");
+let populateCards = require('./dom-builder.js');
 
-getMovies().then (function(data){
-	(createCards(data));
-});
+// getMovies().then (function(data){
+// 	(createCards(data));
+// });
 
 $("#searchView").hide();
 $("#untrackedView").hide();
 $("#favoritesView").hide();
+$("#signin").click(signIn.logInGoogle);
 $("#logOut").click(signIn.logOut);
+
+$("#search").click(function(){
+	console.log("button clicked" );
+	let input = $("#searchBar").val();
+	untrackedResults.getUntracked(input).then(function(data){
+		populateCards.createCards(data);
+	});
+});
 
